bdd5352d890766f9b435a401c1bee92d07f43a9d
thomasgsquinaia <58407522+thomasgsquinaia@users.noreply.github.com>
2021-03-11 20:10:58 -0300

    Up Flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'screens/calculator.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Calculator(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ lib/screens/calculator.dart
@@ -0,0 +1,33 @@
+import 'package:calculadora_basica/models/memory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../components/display.dart';
+import '../components/keyborard.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressd(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
+      home: Column(
+        children: <Widget>[
+          Display(memory.value),
+          Keyboard(_onPressd),
+        ],
+      ),
+    );
+  }
+}
